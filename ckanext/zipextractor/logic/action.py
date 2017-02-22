0483f4315f9c441c1f15cccbfd27dcb46399661f
Gregory von Nessi <gvn@Derp-E.local>
2017-02-22 11:29:51 +1100

    - debugging

--- ckanext/zipextractor/logic/action.py
+++ ckanext/zipextractor/logic/action.py
@@ -292,7 +292,7 @@ def delete_orphaned_resources(context, pkg_dict):
 
     #is_initial_call = not toolkit.asbool(pkg_dict.get('recursion_dict', 'False'))
     tested_ids = set(pkg_dict['resource_ids_to_delete']) | set(pkg_dict.get('ids_already_tested', []))
-    pkg_dict['resources'] = [r for r in pkg_dict['resources'] if r['id'] not in pkg_dict['resources_ids_to_delete']]
+    pkg_dict['resources'] = [r for r in pkg_dict['resources'] if r['id'] not in pkg_dict['resource_ids_to_delete']]
     for res_id in pkg_dict['resource_ids_to_delete']:
         for res in pkg_dict['resources']:
             if res.get('zip_child_of', '') == res_id and res['id'] not in deleted_ids:
